bdfc2bf6a5282eadbdd1d942bd7c090b8ade686c
DANNY <qnv164@gmail.com>
2021-03-20 21:10:39 +0700

    shorter hand

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.js";
 
-function AppTwo() {
-    return <h1>Quynh la vo doi</h1>
-}
 
 ReactDOM.render(
-    <React.Fragment>
-        <App />
-        <AppTwo />
-    </React.Fragment>
-    , document.getElementById("root")
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
 );
